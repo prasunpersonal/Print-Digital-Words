6125d1d012b16d4df8a7c6d2dd4b041d9666f7e8
Prasun Sarkar <prasunsarkar.personal@gmail.com>
2022-07-21 00:49:34 +0530

    Initial commit

---
+++ print_digital_words.cpp
@@ -0,0 +1,242 @@
+// ========================================================
+// Project Name:   Print Digital Words
+// Project For:    Practice
+// Author:         Prasun Sarkar
+// ========================================================
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+void A(){
+    cout<<"  *****"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*********"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t";
+}
+void B(){
+    cout<<"*******"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*******"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*******"<<endl<<"\t";
+}
+void C(){
+    cout<<"  *******"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*"<<endl<<"\t"<<"  *******"<<endl<<"\t";
+}
+void D(){
+    cout<<"*******"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*******"<<endl<<"\t";
+}
+void E(){
+    cout<<"*********"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*******"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*********"<<endl<<"\t";
+}
+void F(){
+    cout<<"*********"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*******"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*"<<endl<<"\t";
+}
+void G(){
+    cout<<"  *******"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*   *****"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"  *****"<<endl<<"\t";
+}
+void H(){
+    cout<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*********"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t";
+}
+void I(){
+    cout<<"*********"<<endl<<"\t"<<"    *"<<endl<<"\t"<<"    *"<<endl<<"\t"<<"    *"<<endl<<"\t"<<"    *"<<endl<<"\t"<<"    *"<<endl<<"\t"<<"*********"<<endl<<"\t";
+}
+void J(){
+    cout<<"        *"<<endl<<"\t"<<"        *"<<endl<<"\t"<<"        *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"  *****"<<endl<<"\t";
+}
+void K(){
+    cout<<"*       *"<<endl<<"\t"<<"*     *"<<endl<<"\t"<<"*   *"<<endl<<"\t"<<"* *"<<endl<<"\t"<<"*   *"<<endl<<"\t"<<"*     *"<<endl<<"\t"<<"*       *"<<endl<<"\t";
+}
+void L(){
+    cout<<"*"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*********"<<endl<<"\t";
+}
+void M(){
+    cout<<"**     **"<<endl<<"\t"<<"* *   * *"<<endl<<"\t"<<"*  * *  *"<<endl<<"\t"<<"*   *   *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t";
+}
+void N(){
+    cout<<"**      *"<<endl<<"\t"<<"* *     *"<<endl<<"\t"<<"*  *    *"<<endl<<"\t"<<"*   *   *"<<endl<<"\t"<<"*    *  *"<<endl<<"\t"<<"*     * *"<<endl<<"\t"<<"*      **"<<endl<<"\t";
+}
+void O(){
+    cout<<"  *****"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"  *****"<<endl<<"\t";
+}
+void P(){
+    cout<<"*******"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*******"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*"<<endl<<"\t";
+}
+void Q(){
+    cout<<"  *****"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*  *    *"<<endl<<"\t"<<"*    *  *"<<endl<<"\t"<<"*      **"<<endl<<"\t"<<"  *******"<<endl<<"\t";
+}
+void R(){
+    cout<<"*******"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*******"<<endl<<"\t"<<"*   *"<<endl<<"\t"<<"*     *"<<endl<<"\t"<<"*       *"<<endl<<"\t";
+}
+void S(){
+    cout<<"  *******"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*"<<endl<<"\t"<<"  *****"<<endl<<"\t"<<"        *"<<endl<<"\t"<<"        *"<<endl<<"\t"<<"*******"<<endl<<"\t";
+}
+void T(){
+    cout<<"*********"<<endl<<"\t"<<"    *"<<endl<<"\t"<<"    *"<<endl<<"\t"<<"    *"<<endl<<"\t"<<"    *"<<endl<<"\t"<<"    *"<<endl<<"\t"<<"    *"<<endl<<"\t";
+}
+void U(){
+    cout<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"  *****"<<endl<<"\t";
+}
+void V(){
+    cout<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"  *   *"<<endl<<"\t"<<"    *"<<endl<<"\t";
+}
+void W(){
+    cout<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*   *   *"<<endl<<"\t"<<"*  * *  *"<<endl<<"\t"<<"* *   * *"<<endl<<"\t"<<"**     **"<<endl<<"\t";
+}
+void X(){
+    cout<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"  *   *"<<endl<<"\t"<<"    *"<<endl<<"\t"<<"  *   *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t";
+}
+void Y(){
+    cout<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"  *   *"<<endl<<"\t"<<"    *"<<endl<<"\t"<<"    *"<<endl<<"\t"<<"    *"<<endl<<"\t";
+}
+void Z(){
+    cout<<"*********"<<endl<<"\t"<<"        *"<<endl<<"\t"<<"      *"<<endl<<"\t"<<"    *"<<endl<<"\t"<<"  *"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*********"<<endl<<"\t";
+}
+void _1(){
+    cout<<"   **"<<endl<<"\t"<<"  * *"<<endl<<"\t"<<" *  *"<<endl<<"\t"<<"*   *"<<endl<<"\t"<<"    *"<<endl<<"\t"<<"    *"<<endl<<"\t"<<"*********"<<endl<<"\t";
+}
+void _2(){
+    cout<<"*******"<<endl<<"\t"<<"        *"<<endl<<"\t"<<"        *"<<endl<<"\t"<<"  *****"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*"<<endl<<"\t"<<"  *******"<<endl<<"\t";
+}
+void _3(){
+    cout<<"*******"<<endl<<"\t"<<"        *"<<endl<<"\t"<<"        *"<<endl<<"\t"<<"  *****"<<endl<<"\t"<<"        *"<<endl<<"\t"<<"        *"<<endl<<"\t"<<"*******"<<endl<<"\t";
+}
+void _4(){
+    cout<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"  *****  "<<endl<<"\t"<<"        *"<<endl<<"\t"<<"        *"<<endl<<"\t"<<"        *"<<endl<<"\t";
+}
+void _6(){
+    cout<<"  *******"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*"<<endl<<"\t"<<"*******"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"  *****"<<endl<<"\t";
+}
+void _7(){
+    cout<<"  *****"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"        *"<<endl<<"\t"<<"        *"<<endl<<"\t"<<"        *"<<endl<<"\t"<<"        *"<<endl<<"\t";
+}
+void _8(){
+    cout<<"  *****"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"  *****"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"  *****"<<endl<<"\t";
+}
+void _9(){
+    cout<<"  *****"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"*       *"<<endl<<"\t"<<"  *******"<<endl<<"\t"<<"        *"<<endl<<"\t"<<"        *"<<endl<<"\t"<<"*******"<<endl<<"\t";
+}
+
+void print(char ch){
+    switch (ch)
+    {
+    case 'A':
+        A();
+        break;
+    case 'B':
+        B();
+        break;
+    case 'C':
+        C();
+        break;
+    case 'D':
+        D();
+        break;
+    case 'E':
+        E();
+        break;
+    case 'F':
+        F();
+        break;
+    case 'G':
+        G();
+        break;
+    case 'H':
+        H();
+        break;
+    case 'I':
+        I();
+        break;
+    case 'J':
+        J();
+        break;
+    case 'K':
+        K();
+        break;
+    case 'L':
+        L();
+        break;
+    case 'M':
+        M();
+        break;
+    case 'N':
+        N();
+        break;
+    case 'O':
+        O();
+        break;
+    case 'P':
+        P();
+        break;
+    case 'Q':
+        Q();
+        break;
+    case 'R':
+        R();
+        break;
+    case 'S':
+        S();
+        break;
+    case 'T':
+        T();
+        break;
+    case 'U':
+        U();
+        break;
+    case 'V':
+        V();
+        break;
+    case 'W':
+        W();
+        break;
+    case 'X':
+        X();
+        break;
+    case 'Y':
+        Y();
+        break;
+    case 'Z':
+        Z();
+        break;
+    case '1':
+        _1();
+        break;
+    case '2':
+        _2();
+        break;
+    case '3':
+        _3();
+        break;
+    case '4':
+        _4();
+        break;
+    case '5':
+        S();
+        break;
+    case '6':
+        _6();
+        break;
+    case '7':
+        _7();
+        break;
+    case '8':
+        _8();
+        break;
+    case '9':
+        _9();
+        break;
+    case '0':
+        O();
+        break;
+    default:
+        for(int i=0;i<7;i++){cout<<endl;}
+        break;
+    }
+}
+
+int main(){
+    string name;
+    cout<<"\tEnter your name: ";
+    getline(cin,name);
+    cout<<endl<<"\t";
+    for (int i = 0; i < name.length(); i++){
+        print(toupper(name.at(i)));
+        cout<<endl;
+    }
+    main();
+    return 0;
+}
